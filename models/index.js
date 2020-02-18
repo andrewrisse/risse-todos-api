3d8cb8e3a24a923ecb725cd53fca93fa812373c7
Andrew Risse <andrewrisse@gmail.com>
2020-02-17 19:53:51 -0500

    fix server not running

--- models/index.js
+++ models/index.js
@@ -1,14 +1,7 @@
-var mongoose = require("mongoose");
-mongoose.set("debug", true);
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://andrewrisse:" + process.env.PASSWORD + "@mycluster-cqqgf.mongodb.net/test?retryWrites=true&w=majority", { 
-	useNewUrlParser: true, 
-	useUnifiedTopology: true  
-}).then(() =>{
-	console.log("Connected to the DB!");
-}).catch(err => {
-	console.log("ERROR: " + err.message);
-});
+var mongoose = require('mongoose');
+mongoose.set('debug', true);
+mongoose.connect('mongodb://localhost/todo-api');
 
-//mongoose.Promise = Promise; // I don't think this is needed
+mongoose.Promise = Promise;
 
 module.exports.Todo = require("./todo");
